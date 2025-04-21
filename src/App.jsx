eec4bff49677e57aee38f833f2a9584936b67b19
jihwan0492421 <tommy040201@gmail.com>
2025-04-21 18:40:21 +0900

    컨테스트api

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,31 @@ import Header from "./components/Header";
 import Menu from "./components/Menu";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart";
+import { MenuProvider } from "./context/menuContext";
+
 
 function App() {
-  const [menu, setMenu] = useState(data.menu);
   const [cart, setCart] = useState([]);
   console.log(cart);
 
   return (
+    <MenuProvider>
     <div>
       <Header />
       <main>
         <Routes>
           <Route
             path="/"
-            element={<Menu menu={menu} cart={cart} setCart={setCart} />}
+            element={<Menu cart={cart} setCart={setCart} />}
           />
           <Route
             path="/cart"
-            element={<Cart menu={menu} cart={cart} setCart={setCart} />}
+            element={<Cart cart={cart} setCart={setCart} />}
           />
         </Routes>
       </main>
     </div>
+    </MenuProvider>
   );
 }
 
